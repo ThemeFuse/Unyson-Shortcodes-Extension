04c15ee3eefe61e6390570ab08a3690a476c7f79
Nicolai Slovineanu <rockalife@gmail.com>
2015-01-12 14:44:06 +0200

    closed #7

--- shortcodes/section/views/view.php
+++ shortcodes/section/views/view.php
@@ -1,6 +1,5 @@
 <?php if (!defined('FW')) die('Forbidden');
 
-$extra_classes = '';
 $bg_color = '';
 if (!empty($atts['background_color'])) {
 	$bg_color = 'background-color:' . $atts['background_color'] . ';';
@@ -11,15 +10,17 @@ if (!empty($atts['background_image']['data']['icon'])) {
 	$bg_image = 'background-image:url(' . $atts['background_image']['data']['icon'] . ');';
 }
 
-$bg_video_data_attr = '';
+$bg_video_data_attr    = '';
+$section_extra_classes = '';
 if (!empty($atts['video'])) {
-	$bg_video_data_attr = 'data-wallpaper-options=' . json_encode(array('source' => array('video' => $atts['video'])));
-	$extra_classes .= ' background-video';
+	$bg_video_data_attr     = 'data-wallpaper-options=' . json_encode(array('source' => array('video' => $atts['video'])));
+	$section_extra_classes .= ' background-video';
 }
 
+$section_style   = ($bg_color || $bg_image) ? 'style="' . $bg_color  .  $bg_image . '"' : '';
 $container_class = (isset($atts['is_fullwidth']) && $atts['is_fullwidth']) ? 'fw-container-fluid' : 'fw-container';
 ?>
-<section class="fw-main-row <?php echo $extra_classes ?>" style="<?php echo $bg_color; ?> <?php echo $bg_image; ?>" <?php echo $bg_video_data_attr; ?>>
+<section class="fw-main-row <?php echo $section_extra_classes ?>" <?php echo $section_style;  ?> <?php echo $bg_video_data_attr; ?>>
 	<div class="<?php echo $container_class; ?>">
 		<?php echo do_shortcode($content); ?>
 	</div>
