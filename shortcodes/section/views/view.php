d1146d0ffd8652c4f67c3941a918fffae397779d
Alexei Gogu <llemurya@gmail.com>
2015-02-19 10:18:18 +0200

    add support for .mp4 .ogg and .webm for section background video, fixed ThemeFuse/Unyson#260 issue

--- shortcodes/section/views/view.php
+++ shortcodes/section/views/view.php
@@ -1,27 +1,33 @@
-<?php if (!defined('FW')) die('Forbidden');
+<?php if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
 
 $bg_color = '';
-if (!empty($atts['background_color'])) {
+if ( ! empty( $atts['background_color'] ) ) {
 	$bg_color = 'background-color:' . $atts['background_color'] . ';';
 }
 
 $bg_image = '';
-if (!empty($atts['background_image']['data']['icon'])) {
+if ( ! empty( $atts['background_image']['data']['icon'] ) ) {
 	$bg_image = 'background-image:url(' . $atts['background_image']['data']['icon'] . ');';
 }
 
 $bg_video_data_attr    = '';
 $section_extra_classes = '';
-if (!empty($atts['video'])) {
-	$bg_video_data_attr     = 'data-wallpaper-options="' . fw_htmlspecialchars(json_encode(array('source' => array('video' => $atts['video'])))) .'"';
+if ( ! empty( $atts['video'] ) ) {
+	$filetype           = wp_check_filetype( $atts['video'] );
+	$filetypes          = array( 'mp4', 'ogg', 'webm' );
+	$filetype           = in_array( $filetype['ext'], $filetypes ) ? $filetype['ext'] : 'video';
+	$bg_video_data_attr = 'data-wallpaper-options="' . fw_htmlspecialchars( json_encode( array( 'source' => array( $filetype => $atts['video'] ) ) ) ) . '"';
 	$section_extra_classes .= ' background-video';
 }
 
-$section_style   = ($bg_color || $bg_image) ? 'style="' . $bg_color  .  $bg_image . '"' : '';
-$container_class = (isset($atts['is_fullwidth']) && $atts['is_fullwidth']) ? 'fw-container-fluid' : 'fw-container';
+$section_style   = ( $bg_color || $bg_image ) ? 'style="' . $bg_color . $bg_image . '"' : '';
+$container_class = ( isset( $atts['is_fullwidth'] ) && $atts['is_fullwidth'] ) ? 'fw-container-fluid' : 'fw-container';
 ?>
-<section class="fw-main-row <?php echo $section_extra_classes ?>" <?php echo $section_style;  ?> <?php echo $bg_video_data_attr; ?>>
+<section
+	class="fw-main-row <?php echo $section_extra_classes ?>" <?php echo $section_style; ?> <?php echo $bg_video_data_attr; ?>>
 	<div class="<?php echo $container_class; ?>">
-		<?php echo do_shortcode($content); ?>
+		<?php echo do_shortcode( $content ); ?>
 	</div>
 </section>
