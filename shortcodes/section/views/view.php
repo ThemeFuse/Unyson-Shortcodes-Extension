5643107f65c4b2f528720b617d36f476c60124a1
moldcraft <moldcraft@email.com>
2015-02-19 17:32:58 +0200

    removed extra space

--- shortcodes/section/views/view.php
+++ shortcodes/section/views/view.php
@@ -25,8 +25,7 @@ if ( ! empty( $atts['video'] ) ) {
 $section_style   = ( $bg_color || $bg_image ) ? 'style="' . $bg_color . $bg_image . '"' : '';
 $container_class = ( isset( $atts['is_fullwidth'] ) && $atts['is_fullwidth'] ) ? 'fw-container-fluid' : 'fw-container';
 ?>
-<section
-	class="fw-main-row <?php echo $section_extra_classes ?>" <?php echo $section_style; ?> <?php echo $bg_video_data_attr; ?>>
+<section class="fw-main-row <?php echo $section_extra_classes ?>" <?php echo $section_style; ?> <?php echo $bg_video_data_attr; ?>>
 	<div class="<?php echo $container_class; ?>">
 		<?php echo do_shortcode( $content ); ?>
 	</div>
