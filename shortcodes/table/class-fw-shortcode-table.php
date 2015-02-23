7c3b2e354d59cb4d8cbecb954a72b21b13e2c219
Pisarenco Gheorghe <pisarenco.gh@gmail.com>
2015-02-23 11:57:25 +0200

    Validated Table Shortcode rendering, Removed error trigger

--- shortcodes/table/class-fw-shortcode-table.php
+++ shortcodes/table/class-fw-shortcode-table.php
@@ -13,20 +13,28 @@ class FW_Shortcode_Table extends FW_Shortcode {
 	}
 
 	private function load_option_type() {
-		require $this->locate_path('/includes/fw-option-type-table/class-fw-option-type-table.php');
-		require $this->locate_path('/includes/fw-option-type-textarea-cell/class-fw-option-type-textarea-cell.php');
+		require $this->locate_path( '/includes/fw-option-type-table/class-fw-option-type-table.php' );
+		require $this->locate_path( '/includes/fw-option-type-textarea-cell/class-fw-option-type-textarea-cell.php' );
 	}
 
-	protected function _render($atts, $content = null, $tag = '') {
-		$view_file = $this->locate_path('/views/' . $atts['table']['header_options']['table_purpose'] . '.php');
-		if (!$view_file) {
-			trigger_error(
-				sprintf(__('No default view (views/view.php) found for shortcode: %s', 'fw'), $tag),
-				E_USER_ERROR
-			);
+	protected function _render( $atts, $content = null, $tag = '' ) {
+
+		if (
+			! isset( $atts['table'] )
+			|| ! isset( $atts['table']['header_options'] )
+			|| ! isset( $atts['table']['header_options']['table_purpose'] )
+		) {
+			$view_file = $this->get_declared_path( '/views/tabular.php' );
+		} else {
+			$view_file = $this->locate_path( '/views/' . $atts['table']['header_options']['table_purpose'] . '.php' );
+		}
+
+		if ( ! $view_file ) {
+			$view_file = $this->get_declared_path( '/views/tabular.php' );
 		}
 
 		$this->enqueue_static();
+
 		return fw_render_view( $view_file, array(
 			'atts'    => $atts,
 			'content' => $content,
