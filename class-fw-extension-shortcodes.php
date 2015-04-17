90e527d8b6b548f93d742ec89ffb078a9bf09efd
moldcraft <moldcraft@email.com>
2015-04-17 22:30:41 +0300

    fix for https://github.com/ThemeFuse/Unyson/issues/275

--- class-fw-extension-shortcodes.php
+++ class-fw-extension-shortcodes.php
@@ -103,11 +103,21 @@ class FW_Extension_Shortcodes extends FW_Extension
 			return;
 		}
 
-		$pattern = get_shortcode_regex();
-		preg_replace_callback( "/$pattern/s", array( $this, 'enqueue_shortcode_static'), $post->post_content );
+		$this->enqueue_shortcodes_static($post->post_content);
+	}
+
+	private function enqueue_shortcodes_static( $content ) {
+		preg_replace_callback( '/'. get_shortcode_regex() .'/s', array( $this, 'enqueue_shortcode_static'), $content );
 	}
 
 	private function enqueue_shortcode_static( $shortcode ) {
+		/**
+		 * Remember the enqueued shortcodes and prevent enqueue static multiple times.
+		 * There is no sense to call enqueue_static() multiple times
+		 * because there is no dynamic data passed to it.
+		 */
+		static $enqueued_shortcodes = array();
+
 		// allow [[foo]] syntax for escaping a tag
 		if ( $shortcode[1] == '[' && $shortcode[6] == ']' ) {
 			return;
@@ -116,7 +126,24 @@ class FW_Extension_Shortcodes extends FW_Extension
 		$tag = $shortcode[2];
 
 		if ( ! is_null( $this->get_shortcode( $tag ) ) ) {
-			$this->get_shortcode( $tag )->_enqueue_static();
+			if (!isset($enqueued_shortcodes[$tag])) {
+				$this->get_shortcode($tag)->_enqueue_static();
+				$enqueued_shortcodes[$tag] = true;
+			}
+
+			do_action('fw_ext_shortcodes_enqueue_static:'. $tag, array(
+				/**
+				 * Transform to array:
+				 * $attr = shortcode_parse_atts( $data['atts_string'] );
+				 *
+				 * By default it's not transformed, but sent as raw string,
+				 * to prevent useless computation for every shortcode,
+				 * because this action may be used very rare and only for a specific shortcode.
+				 */
+				'atts_string' => $shortcode[3],
+			));
+
+			$this->enqueue_shortcodes_static($shortcode[5]); // inner shortcodes
 		}
 	}
 }
