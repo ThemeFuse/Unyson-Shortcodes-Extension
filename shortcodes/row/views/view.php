678e7974dba7d7461b910da6ff413350ff9f4319
moldcraft <moldcraft@email.com>
2016-03-15 18:13:18 +0200

    use row class from builder config

--- shortcodes/row/views/view.php
+++ shortcodes/row/views/view.php
@@ -1,6 +1,7 @@
-<?php if (!defined('FW')) die('Forbidden'); ?>
-
-<div class="fw-row">
+<?php if (!defined('FW')) die('Forbidden');
+$row_class = ($row_class  = fw_ext('builder')->get_config('grid.row.class')) ? $row_class : 'fw-row';
+?>
+<div class="<?php echo esc_attr($row_class); ?>">
 	<?php echo do_shortcode($content); ?>
 </div>
 
