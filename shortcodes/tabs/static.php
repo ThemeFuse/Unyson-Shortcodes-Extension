a225c168a400897d70f6f46c4d848c95d9a7cc7d
moldcraft <moldcraft@email.com>
2016-07-19 14:49:23 +0300

    fixed bugs in tabs style

--- shortcodes/tabs/static.php
+++ shortcodes/tabs/static.php
@@ -3,11 +3,11 @@
 $shortcodes_extension = fw_ext('shortcodes');
 wp_enqueue_style(
 	'fw-shortcode-tabs',
-	$shortcodes_extension->get_declared_URI('/shortcodes/tabs/static/css/styles.css')
+	$shortcodes_extension->get_uri('/shortcodes/tabs/static/css/styles.css')
 );
 wp_enqueue_script(
 	'fw-shortcode-tabs',
-	$shortcodes_extension->get_declared_URI('/shortcodes/tabs/static/js/scripts.js'),
+	$shortcodes_extension->get_uri('/shortcodes/tabs/static/js/scripts.js'),
 	array('jquery-ui-tabs'),
 	false,
 	true
