854fc50d6adc81d29cc6f6772e233c4bad84e251
moldcraft <moldcraft@email.com>
2016-07-28 20:03:56 +0300

    Version 1.3.18

--- manifest.php
+++ manifest.php
@@ -1,7 +1,7 @@
 <?php if (!defined('FW')) die('Forbidden');
 
 $manifest = array(
-	'version'       => '1.3.17',
+	'version'       => '1.3.18',
 	'display'       => false,
 	'standalone'    => true,
 	'requirements'  => array(
