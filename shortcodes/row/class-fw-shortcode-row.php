7ec3f41995d59a6270c258e8293a5037e9d10b3d
moldcraft <moldcraft@email.com>
2016-08-29 20:29:34 +0300

    register PB items only when needed

--- shortcodes/row/class-fw-shortcode-row.php
+++ shortcodes/row/class-fw-shortcode-row.php
@@ -7,6 +7,13 @@ class FW_Shortcode_Row extends FW_Shortcode
 	 */
 	public function _init()
 	{
+		add_action(
+			'fw_option_type_builder:page-builder:register_items',
+			array($this, '_action_register_builder_item_types')
+		);
+	}
+
+	public function _action_register_builder_item_types() {
 		if (fw_ext('page-builder')) {
 			require $this->get_declared_path('/includes/page-builder-row-item/class-page-builder-row-item.php');
 		}
--- shortcodes/section/class-fw-shortcode-section.php
+++ shortcodes/section/class-fw-shortcode-section.php
@@ -7,6 +7,13 @@ class FW_Shortcode_Section extends FW_Shortcode
 	 */
 	public function _init()
 	{
+		add_action(
+			'fw_option_type_builder:page-builder:register_items',
+			array($this, '_action_register_builder_item_types')
+		);
+	}
+
+	public function _action_register_builder_item_types() {
 		if (fw_ext('page-builder')) {
 			require $this->get_declared_path('/includes/page-builder-section-item/class-page-builder-section-item.php');
 		}
