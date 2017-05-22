5febf089e277dbaeadbb33a261b007f283501639
Gheorghe <pisarenco.gh@gmail.com>
2017-05-22 11:02:04 +0300

    Code refactoring

--- includes/class-fw-shortcode.php
+++ includes/class-fw-shortcode.php
@@ -1,7 +1,8 @@
-<?php if (!defined('FW')) die('Forbidden');
+<?php if ( ! defined( 'FW' ) ) {
+	die( 'Forbidden' );
+}
 
-class FW_Shortcode
-{
+class FW_Shortcode {
 	private $tag;
 	private $path;
 	private $uri;
@@ -10,8 +11,7 @@ class FW_Shortcode
 	private $options;
 	private $config;
 
-	final public function __construct($args)
-	{
+	final public function __construct( $args ) {
 		$this->tag           = $args['tag'];
 		$this->path          = $args['path'];
 		$this->uri           = $args['uri'];
@@ -21,36 +21,36 @@ class FW_Shortcode
 		$this->_init();
 	}
 
-	protected function _init()
-	{
+	protected function _init() {
 	}
 
 	/**
 	 * Gets the shortcodes' tag (id)
 	 * @return string
 	 */
-	final public function get_tag()
-	{
+	final public function get_tag() {
 		return $this->tag;
 	}
 
 	/**
 	 * Gets the path at which the shortcode is located
+	 *
 	 * @param string $rel_path A string to append to the path like '/views/view.php'
+	 *
 	 * @return string
 	 */
-	final public function get_declared_path($rel_path = '')
-	{
+	final public function get_declared_path( $rel_path = '' ) {
 		return $this->path . $rel_path;
 	}
 
 	/**
 	 * Gets the uri at which the shortcode is located
+	 *
 	 * @param string $rel_path A string to append to the uri like '/views/view.php'
+	 *
 	 * @return string
 	 */
-	final public function get_declared_URI($rel_path = '')
-	{
+	final public function get_declared_URI( $rel_path = '' ) {
 		return $this->uri . $rel_path;
 	}
 
@@ -60,13 +60,13 @@ class FW_Shortcode
 	 * This allows to find overridden files like the view and static files
 	 *
 	 * @param $rel_path string A string to append to the path like '/views/view.php'
+	 *
 	 * @return string|bool The path if it was found or false otherwise
 	 */
-	final public function locate_path($rel_path = '')
-	{
-		foreach (array_merge($this->rewrite_paths, array($this->path)) as $path) {
+	final public function locate_path( $rel_path = '' ) {
+		foreach ( array_merge( $this->rewrite_paths, array( $this->path ) ) as $path ) {
 			$actual_path = $path . $rel_path;
-			if (file_exists($actual_path)) {
+			if ( file_exists( $actual_path ) ) {
 				return $actual_path;
 			}
 		}
@@ -76,10 +76,11 @@ class FW_Shortcode
 
 	/**
 	 * @param string $append
+	 *
 	 * @return string
 	 * @since 1.3.5
 	 */
-	public function get_uri($append = '') {
+	public function get_uri( $append = '' ) {
 		return $this->uri . $append;
 	}
 
@@ -89,10 +90,10 @@ class FW_Shortcode
 	 * This allows to find uris to overridden files like the view and static files
 	 *
 	 * @param $rel_path string A string to append to the path like '/views/view.php'
+	 *
 	 * @return string|bool The path if it was found or false otherwise
 	 */
-	final public function locate_URI($rel_path = '')
-	{
+	final public function locate_URI( $rel_path = '' ) {
 		$paths = array_merge( $this->rewrite_paths, array( $this->path ) );
 		$uris  = array_merge( $this->rewrite_uris, array( $this->uri ) );
 		foreach ( $paths as $key => $path ) {
@@ -105,33 +106,32 @@ class FW_Shortcode
 		return false;
 	}
 
-	public function get_config($key = null)
-	{
-		if (!$this->config) {
-			$config_path = $this->locate_path('/config.php');
-			if ($config_path) {
-				$vars = fw_get_variables_from_file($config_path, array('cfg' => null));
+	public function get_config( $key = null ) {
+		if ( ! $this->config ) {
+			$config_path = $this->locate_path( '/config.php' );
+			if ( $config_path ) {
+				$vars         = fw_get_variables_from_file( $config_path, array( 'cfg' => null ) );
 				$this->config = $vars['cfg'];
 			}
 		}
 
-		if (!is_array($this->config)) {
+		if ( ! is_array( $this->config ) ) {
 			return null;
 		} else {
-			return $key === null ? $this->config : fw_akg($key, $this->config);
+			return $key === null ? $this->config : fw_akg( $key, $this->config );
 		}
 	}
 
-	public function get_options()
-	{
-		if (!$this->options) {
-			$options_path = $this->locate_path('/options.php');
-			if ($options_path) {
-				$vars = fw_get_variables_from_file($options_path, array('options' => null));
+	public function get_options() {
+		if ( ! $this->options ) {
+			$options_path = $this->locate_path( '/options.php' );
+			if ( $options_path ) {
+				$vars          = fw_get_variables_from_file( $options_path, array( 'options' => null ) );
 				$this->options = $vars['options'];
 			}
 		}
-		return apply_filters('fw_shortcode_get_options', $this->options, $this->tag);
+
+		return apply_filters( 'fw_shortcode_get_options', $this->options, $this->tag );
 	}
 
 	/**
@@ -145,20 +145,21 @@ class FW_Shortcode
 	 * @param $atts
 	 * @param null $content
 	 * @param string $tag deprecated
+	 *
 	 * @return string
 	 */
-	final public function render($atts, $content = null, $tag = '')
-	{
-		if (empty($atts)) {
+	final public function render( $atts, $content = null, $tag = '' ) {
+		if ( empty( $atts ) ) {
 			$atts = array();
 		} else {
-			if (version_compare(fw_ext('shortcodes')->manifest->get_version(), '1.3.0', '>=')) {
+			if ( version_compare( fw_ext( 'shortcodes' )->manifest->get_version(), '1.3.0', '>=' ) ) {
 				/**
 				 * @var WP_Post $post
 				 */
 				global $post;
 
-				$atts = fw_ext_shortcodes_decode_attr($atts, $this->tag,
+				$atts = fw_ext_shortcodes_decode_attr( $atts,
+					$this->tag,
 					/**
 					 * This was required, but sometimes the shortcode is rendered manually from code and
 					 * very often is used PageBuilder encoder which doesn't require/need post id
@@ -166,58 +167,61 @@ class FW_Shortcode
 					$post ? $post->ID : 0
 				);
 
-				if (is_wp_error($atts)) {
+				if ( is_wp_error( $atts ) ) {
 					return '<p>Shortcode attributes decode error: ' . $atts->get_error_message() . '</p>';
 				}
 			} else {
 				/**
 				 * @deprecated Since Shortcodes 1.3.0
 				 */
-				$atts = apply_filters('fw_shortcode_atts', $atts, $content, $this->tag);
+				$atts = apply_filters( 'fw_shortcode_atts', $atts, $content, $this->tag );
 			}
 		}
 
-		return $this->_render($atts, $content);
+		return $this->_render( $atts, $content );
 	}
 
 	/**
 	 * @param $atts
 	 * @param null $content
 	 * @param string $tag deprecated
+	 *
 	 * @return string
 	 */
-	protected function _render($atts, $content = null, $tag = '')
-	{
-		$view_file = $this->locate_path('/views/view.php');
-		if (!$view_file) {
+	protected function _render( $atts, $content = null, $tag = '' ) {
+		$view_file = $this->locate_path( '/views/view.php' );
+		if ( ! $view_file ) {
 			trigger_error(
-				sprintf(__('No default view (views/view.php) found for shortcode: %s', 'fw'), $this->tag),
+				sprintf( __( 'No default view (views/view.php) found for shortcode: %s', 'fw' ), $this->tag ),
 				E_USER_ERROR
 			);
 		}
 
 		$this->enqueue_static();
 
-		$view_extra = apply_filters('fw_shortcode_render_view', array(
-			'before' => '',
-			'after' => '',
-		), $atts, $this->tag);
+		$view_extra = apply_filters( 'fw_shortcode_render_view',
+			array(
+				'before' => '',
+				'after'  => '',
+			),
+			$atts,
+			$this->tag );
 
 		return
 			$view_extra['before'] .
-			fw_render_view($view_file, array(
-				'atts'    => apply_filters('fw_shortcode_render_view:atts', $atts, $this->tag),
-				'content' => $content,
-				'tag'     => $this->tag
-			)) .
+			fw_render_view( $view_file,
+				array(
+					'atts'    => apply_filters( 'fw_shortcode_render_view:atts', $atts, $this->tag ),
+					'content' => $content,
+					'tag'     => $this->tag
+				) ) .
 			$view_extra['after'];
 	}
 
-	protected function enqueue_static()
-	{
-		$static_file = $this->locate_path('/static.php');
-		if ($static_file) {
-			fw_include_file_isolated($static_file);
+	protected function enqueue_static() {
+		$static_file = $this->locate_path( '/static.php' );
+		if ( $static_file ) {
+			fw_include_file_isolated( $static_file );
 		}
 	}
 }
