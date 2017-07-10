bb7f165f12aeba61cd3be60dc63e30239978d55e
Gheorghe <pisarenco.gh@gmail.com>
2017-07-10 13:27:26 +0300

    New version 1.3.27

--- manifest.php
+++ manifest.php
@@ -1,7 +1,7 @@
 <?php if (!defined('FW')) die('Forbidden');
 
 $manifest = array(
-	'version'       => '1.3.26',
+	'version'       => '1.3.27',
 	'display'       => false,
 	'standalone'    => true,
 	'requirements'  => array(
