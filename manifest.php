83a84bf4e66a0ca5bbffa8f7c7dfb94892bcbe41
Viorel <eremiaviorel0@gamil.com>
2018-07-03 19:46:26 +0300

    Version

--- manifest.php
+++ manifest.php
@@ -1,7 +1,7 @@
 <?php if (!defined('FW')) die('Forbidden');
 
 $manifest = array(
-	'version'       => '1.3.30',
+	'version'       => '1.3.31',
 	'display'       => false,
 	'standalone'    => true,
 	'requirements'  => array(
