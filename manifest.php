0fa933b7d52824ab5ea201361762767b37cd9d2e
Viorel <eremiaviorel0@gmail.com>
2022-02-18 17:38:02 +0200

    New version 1.3.32

--- manifest.php
+++ manifest.php
@@ -1,7 +1,7 @@
 <?php if (!defined('FW')) die('Forbidden');
 
 $manifest = array(
-	'version'       => '1.3.31',
+	'version'       => '1.3.32',
 	'display'       => false,
 	'standalone'    => true,
 	'requirements'  => array(
